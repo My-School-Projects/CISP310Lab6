13c9235a5da049e0563d8f2954853912efd26387
Michael Dorst <code@mikefdorst.com>
2016-10-06 16:56:18 -0700

    Initial Commit

---
+++ windows32/main.asm
@@ -0,0 +1,22 @@
+.586
+.MODEL FLAT	; only 32 bit addresses, no segment:offset
+
+INCLUDE io.h   ; header file for input/output
+
+.STACK 4096	   ; allocate 4096 bytes for the stack
+
+.DATA
+
+	
+
+.CODE
+_MainProc PROC
+	
+	
+
+	mov eax, 0							; exit with return code 0
+	
+	ret
+_MainProc ENDP
+
+END   ; end of source code
