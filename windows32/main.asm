0cfa3a0b94a025c7b71a9ad6c72e7bd8ce44c49e
Michael Dorst <code@mikefdorst.com>
2016-10-06 17:54:34 -0700

    Implement Euclid's algorithm

--- windows32/main.asm
+++ windows32/main.asm
@@ -6,15 +6,67 @@ INCLUDE io.h   ; header file for input/output
 .STACK 4096	   ; allocate 4096 bytes for the stack
 
 .DATA
-
 	
+	; We are using DWORD sizes, because the problem spec says to.
+	
+	; These prompts will be displayed when asking for numbers
+	; They are null terminated because the input macro expects a C-string
+	number1Prompt BYTE "Please input a positive integer", 0
+	number2Prompt BYTE "Please input another positive integer", 0
+
+	; inputString will be used to store the user's inputs in ASCII coded decimal form.
+	; It is 11 characters + Null terminator, because the most characters you could need
+	; to express a signed double word is 11 characters (-2147483648d is the longest number
+	; representable as a DWORD)
+	inputString BYTE 11 DUP ("X"), 0
+
+	; gcdString will be used with dtoa, which expects 11 characters + Null terminator
+	gcdString BYTE 11 DUP ("X"), 0	; String to store gcd in decimal for output
+	
+	; gcdOutputLabel will be used to label the gcd when it is displayed
+	gcdOutputLabel BYTE "Greatest Common Divisor"
+
 
 .CODE
 _MainProc PROC
 	
-	
+	; gcd := number1
+	; remainder := number2
+	; loopStart
+	;   dividend := gcd
+	;   gcd := remainder
+	;   remainder := dividend mod gcd
+	; until(remainder = 0)
 
-	mov eax, 0							; exit with return code 0
+	; We are using DWORD sizes, because the problem spec says to.
+	; ebx = gcd
+	; ecx = remainder
+	; eax = dividend
+
+	input number1Prompt, inputString, 11	; get input from user (maximum 11 characters, as explained above at inputString)
+	atod inputString						; convert input from ASCII coded decimal to binary integer (stored in EAX)
+	mov ebx, eax							; gcd := user input
+
+	input number2Prompt, inputString, 11	; get input from user
+	atod inputString						; convert input from string to binary integer (stored in EAX)
+	mov ecx, eax							; remainder := user input
+
+loopStart:
+	mov eax, ebx							; dividend := gcd
+	mov ebx, ecx							; gcd := remainder
+	
+	cdq										; prepare for division
+	div ebx									; divide dividend by gcd (to get dividend mod gcd)
+	mov ecx, edx							; remainder := dividend mod gcd
+	cmp ecx, 0								; remainder = 0?
+	jnz loopStart							; if (remainder != 0) goto loopStart
+loopEnd:
+	
+	dtoa gcdString, ebx						; convert gcd to string (for output)
+	output gcdOutputLabel, gcdString		; output gcd
+
+
+	mov eax, 0								; exit with return code 0
 	
 	ret
 _MainProc ENDP
