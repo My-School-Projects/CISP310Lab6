82e38c3bc062bd72d9fbcb66338a773862bd8f1a
Michael Dorst <code@mikefdorst.com>
2016-10-06 18:43:02 -0700

    Fix truncation bug

--- windows32/main.asm
+++ windows32/main.asm
@@ -8,17 +8,19 @@ INCLUDE io.h   ; header file for input/output
 .DATA
 	
 	; We are using DWORD sizes, because the problem spec says to.
+	; We are using unsing unsigned because the problem says the two numbers are positive
 	
 	; These prompts will be displayed when asking for numbers
 	; They are null terminated because the input macro expects a C-string
 	number1Prompt BYTE "Please input a positive integer", 0
 	number2Prompt BYTE "Please input another positive integer", 0
 
-	; inputString will be used to store the user's inputs in ASCII coded decimal form.
-	; It is 11 characters + Null terminator, because the most characters you could need
-	; to express a signed double word is 10 characters (4294967295d is the longest number
-	; representable as a DWORD)
-	inputString BYTE 10 DUP ("X"), 0
+	; inputString will be used to store the user's inputs in ASCII.
+	; It is 11 characters long because the longest input will be "4294967295" (11 characters) and the input
+	; macro appends a trailing null (11)
+	; we tried this with 10 bytes, but because of the trailing null the final character of a 10 character number would get cut off,
+	; leaving only 9 characters to represent the number.
+	inputString BYTE 11 DUP ("X")
 
 	; gcdString will be used with dtoa, which expects 11 characters + Null terminator
 	gcdString BYTE 11 DUP ("X"), 0	; String to store gcd in decimal for output
@@ -43,11 +45,11 @@ _MainProc PROC
 	; ecx = remainder
 	; eax = dividend
 
-	input number1Prompt, inputString, 10	; get input from user (maximum 10 characters, as explained above at inputString)
+	input number1Prompt, inputString, 11	; get input from user (maximum 11 characters, as explained above at inputString)
 	atod inputString						; convert input from ASCII coded decimal to binary integer (stored in EAX)
 	mov ebx, eax							; gcd := user input
 
-	input number2Prompt, inputString, 10	; get input from user
+	input number2Prompt, inputString, 11	; get input from user
 	atod inputString						; convert input from string to binary integer (stored in EAX)
 	mov ecx, eax							; remainder := user input
 
